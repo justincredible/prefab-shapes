1277ca9e0a179a8c310b5b5c18d83bfdd5e71262
Justin Symonds <justinjfsymonds@hotmail.com>
2025-10-05 22:33:46 -0700

    Add doc comments

--- src/shapes/mod.rs
+++ src/shapes/mod.rs
@@ -16,13 +16,19 @@ pub enum Shape<C: Float, I: Unsigned> {
     },
 }
 
+/// The handedness of the coordinate system, with:
+/// <br>&emsp;+X rightward
+/// <br>&emsp;+Y upward
 #[derive(Default, Clone, Copy, PartialEq, Eq)]
 pub enum Chirality {
+    /// +Z forward
     Left,
+    /// +Z backward
     #[default]
     Right,
 }
 
+/// Which direction defines the front face.
 #[derive(Default, Clone, Copy, PartialEq, Eq)]
 pub enum Winding {
     Clockwise,
@@ -30,9 +36,14 @@ pub enum Winding {
     Counterclockwise,
 }
 
+/// Defines the [`Shape`]'s local/object space.
+///
+/// Generally not sufficient for direct use in the Normalized Device Coordinates (NDC) space.
 #[derive(Default, Clone, Copy)]
 pub struct Orientation {
+    /// Whether the coordinate space is left- or right-handed.
     pub chirality: Chirality,
+    /// How the vertex order defines the front and back faces.
     pub winding: Winding,
 }
 
@@ -54,13 +65,18 @@ impl Orientation {
     }
 }
 
+/// Passed to [`Shaper::make()`] to configure the returned [`Shape`].
 #[derive(Default, Clone, Copy)]
 pub struct Configuration {
+    /// Controls the coordinate space [`Chirality`] and primitive [`Winding`] order.
     pub orientation: Orientation,
+    /// Whether the [`Shape`] should include normal data.
     pub generate_normals: bool,
+    /// Provides index data as triangle strips, if possible.
     pub prefer_strips: bool,
 }
 
+/// Construct a well-defined [`Shape`] on demand.
 pub trait Shaper<C: Float, I: Unsigned> {
     fn make(&self, _request: Configuration) -> Shape<C, I> {
         Shape::Triangles { vertices: vec!(), indices: vec!() }
