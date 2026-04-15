fa353318db596586164e07cec84210e1c809c45b
Justin Symonds <justinjfsymonds@hotmail.com>
2026-04-14 22:56:28 -0700

    Use new height defn changes on icosahedron

--- src/prefab/platonic_solids.rs
+++ src/prefab/platonic_solids.rs
@@ -205,34 +205,29 @@ where
                 let f0 = zero();
                 let f1 = one::<C>();
                 let f2 = cast::<_, C>(2.).unwrap();
-                let f3 = cast::<_, C>(3.).unwrap();
                 let f10 = cast::<_, C>(10.).unwrap();
                 let fh = cast::<_, C>(0.5).unwrap();
                 let fq = cast::<_, C>(0.25).unwrap();
-                let ft = cast::<_, C>(0.1).unwrap();
                 let sr5 = cast::<_, C>(5.).unwrap().sqrt();
 
-                let mid = fq * (f10 + f2 * sr5).sqrt();
-                let top = fq * (f10 - f2 * sr5).sqrt();
                 let width = fq * (f1 + sr5); // phi/2
-                let depth = top + mid;
-                let center = fq * (f2 + sr5) / depth;
-                let radius = fq * (f3 + sr5) / depth;
-
-                let y_diff = (fh - ft * sr5).sqrt();
-                let half_middle = fh * (fh + ft * sr5).sqrt();
+                let center = fh * (f1 + f2 / sr5).sqrt();
+                let radius = fh * (f2 + f2 / sr5).sqrt();
+                let mid = fq * (f2 - f2 / sr5).sqrt();
+                let y_diff = (fh - sr5 / f10).sqrt();
+                let half_middle = fh * (fh + sr5 / f10).sqrt();
 
                 let vertices = vec![
                     [f0, half_middle + y_diff, f0],
                     [f0, half_middle, -radius],
-                    [-width, half_middle, -radius + top],
-                    [width, half_middle, -radius + top],
+                    [-width, half_middle, -mid],
+                    [width, half_middle, -mid],
                     [-fh, half_middle, center],
                     [fh, half_middle, center],
                     [-fh, -half_middle, -center],
                     [fh, -half_middle, -center],
-                    [-width, -half_middle, radius - top],
-                    [width, -half_middle, radius - top],
+                    [-width, -half_middle, mid],
+                    [width, -half_middle, mid],
                     [f0, -half_middle, radius],
                     [f0, -half_middle - y_diff, f0],
                 ];
