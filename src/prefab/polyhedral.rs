52bd95ee0cb5d4ed9ce4299789ec0fa533978047
Justin Symonds <justinjfsymonds@hotmail.com>
2026-05-09 16:41:44 -0700

    Remove unnecessary comments

--- src/prefab/polyhedral.rs
+++ src/prefab/polyhedral.rs
@@ -1,8 +1,6 @@
 pub(super) trait Polyhedral {
-    /// Required
     fn edges(&self) -> Vec<Vec<usize>>;
 
-    /// Provided
     fn vertex_count(&self) -> usize {
         self.edges().len()
     }
