b8bccfe0c749760fa43522a876da53ebf50d1833
Justin Symonds <justinjfsymonds@hotmail.com>
2026-05-09 16:47:57 -0700

    Remove unnecessary attributes

--- src/prefab/mod.rs
+++ src/prefab/mod.rs
@@ -1,10 +1,7 @@
 pub mod kepler_poinsot;
-#[doc(hidden)]
 mod pentagonal;
 pub mod platonic_solid;
 pub mod polygon;
-#[doc(hidden)]
 mod polyhedral;
-#[doc(hidden)]
 #[allow(unused)]
-pub mod unit_test;
+mod unit_test;
