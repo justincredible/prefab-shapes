ce98e0afc380509f37a25f0538f949c82101e19c
Justin Symonds <justinjfsymonds@hotmail.com>
2026-05-17 00:34:16 -0700

    Move unsafe blocks

--- example2/src/main.rs
+++ example2/src/main.rs
@@ -1,49 +1,56 @@
 use glow::*;
 
 fn main() {
-    unsafe {
-        // Create a context from a sdl2 window
-        let (gl, window, mut event_loop, _context) = create_sdl2_context();
+    // Create a context from a sdl2 window
+    let (gl, window, mut event_loop, _context) = create_sdl2_context();
 
-        // Create a shader program from source
-        let program = create_program(
-            &gl,
-            VERTEX_SHADER_SOURCE,
-            FRAGMENT_SHADER_SOURCE,
-            GEOMETRY_SHADER_SOURCE,
-        );
-        gl.use_program(Some(program));
+    // Create a shader program from source
+    let program = create_program(
+        &gl,
+        VERTEX_SHADER_SOURCE,
+        FRAGMENT_SHADER_SOURCE,
+        GEOMETRY_SHADER_SOURCE,
+    );
+    unsafe { gl.use_program(Some(program)); }
 
-        // Create a vertex buffer and vertex array object
-        let (vbo, vao) = create_vertex_buffer(&gl);
+    // Create a vertex buffer and vertex array object
+    let (vbo, vao) = create_vertex_buffer(&gl);
 
-        // Upload some uniforms
-        set_uniform(&gl, program);
+    // Upload some uniforms
+    set_uniform_matrix(
+        &gl,
+        program,
+        "transform",
+        &[1.0f32, 0., 0., 0., 0., 1., 0., 0., 0., 0., 1., 0., 0., 0., 0., 1.],
+    );
 
-        gl.clear_color(0.0, 0.0, 0.0, 1.0);
+    unsafe { gl.clear_color(0.0, 0.0, 0.0, 1.0); }
 
-        'render: loop {
-            {
-                for event in event_loop.poll_iter() {
-                    if let sdl2::event::Event::Quit { .. } = event {
-                        break 'render;
-                    }
+    'render: loop {
+        {
+            for event in event_loop.poll_iter() {
+                if let sdl2::event::Event::Quit { .. } = event {
+                    break 'render;
                 }
             }
+        }
 
+        unsafe {
             gl.clear(glow::COLOR_BUFFER_BIT);
             gl.draw_arrays(glow::TRIANGLES, 0, 3);
             window.gl_swap_window();
         }
+    }
 
-        // Clean up
+    // Clean up
+    unsafe {
         gl.delete_program(program);
         gl.delete_vertex_array(vao);
         gl.delete_buffer(vbo)
     }
 }
 
-unsafe fn create_sdl2_context() -> (
+fn create_sdl2_context() -> (
     glow::Context,
     sdl2::video::Window,
     sdl2::EventPump,
@@ -62,19 +69,19 @@ unsafe fn create_sdl2_context() -> (
         .build()
         .unwrap();
     let gl_context = window.gl_create_context().unwrap();
-    let gl = glow::Context::from_loader_function(|s| video.gl_get_proc_address(s) as *const _);
+    let gl = unsafe { glow::Context::from_loader_function(|s| video.gl_get_proc_address(s) as *const _) };
     let event_loop = sdl.event_pump().unwrap();
 
     (gl, window, event_loop, gl_context)
 }
 
-unsafe fn create_program(
+fn create_program(
     gl: &glow::Context,
     vertex_shader_source: &str,
     fragment_shader_source: &str,
     geometry_shader_source: &str,
 ) -> NativeProgram {
-    let program = gl.create_program().expect("Cannot create program");
+    let program = unsafe { gl.create_program().expect("Cannot create program") };
 
     let shader_sources = [
         (glow::VERTEX_SHADER, vertex_shader_source),
@@ -85,60 +92,70 @@ unsafe fn create_program(
     let mut shaders = Vec::with_capacity(shader_sources.len());
 
     for (shader_type, shader_source) in shader_sources.iter() {
-        let shader = gl
+        let shader = unsafe { gl
             .create_shader(*shader_type)
-            .expect("Cannot create shader");
-        gl.shader_source(shader, shader_source);
-        gl.compile_shader(shader);
-        if !gl.get_shader_compile_status(shader) {
-            panic!("{}", gl.get_shader_info_log(shader));
+            .expect("Cannot create shader") };
+        unsafe {
+            gl.shader_source(shader, shader_source);
+            gl.compile_shader(shader);
+            if !gl.get_shader_compile_status(shader) {
+                panic!("{}", gl.get_shader_info_log(shader));
+            }
+            gl.attach_shader(program, shader);
         }
-        gl.attach_shader(program, shader);
         shaders.push(shader);
     }
 
-    gl.link_program(program);
-    if !gl.get_program_link_status(program) {
-        panic!("{}", gl.get_program_info_log(program));
+    unsafe {
+        gl.link_program(program);
+        if !gl.get_program_link_status(program) {
+            panic!("{}", gl.get_program_info_log(program));
+        }
     }
 
     for shader in shaders {
-        gl.detach_shader(program, shader);
-        gl.delete_shader(shader);
+        unsafe {
+            gl.detach_shader(program, shader);
+            gl.delete_shader(shader);
+        }
     }
 
     program
 }
 
-unsafe fn create_vertex_buffer(gl: &glow::Context) -> (NativeBuffer, NativeVertexArray) {
+fn create_vertex_buffer(gl: &glow::Context) -> (NativeBuffer, NativeVertexArray) {
     // This is a flat array of f32s that are to be interpreted as vec3s.
     let triangle_vertices = [0.0f32, 0.5, 0., -0.5, -0.5, 0., 0.5, -0.5, 0.];
-    let triangle_vertices_u8: &[u8] = core::slice::from_raw_parts(
+    let triangle_vertices_u8: &[u8] = unsafe { core::slice::from_raw_parts(
         triangle_vertices.as_ptr() as *const u8,
         triangle_vertices.len() * core::mem::size_of::<f32>(),
-    );
+    ) };
 
     // We construct a buffer and upload the data
-    let vbo = gl.create_buffer().unwrap();
-    gl.bind_buffer(glow::ARRAY_BUFFER, Some(vbo));
-    gl.buffer_data_u8_slice(glow::ARRAY_BUFFER, triangle_vertices_u8, glow::STATIC_DRAW);
+    let vbo = unsafe { gl.create_buffer().unwrap() };
+    unsafe {
+        gl.bind_buffer(glow::ARRAY_BUFFER, Some(vbo));
+        gl.buffer_data_u8_slice(glow::ARRAY_BUFFER, triangle_vertices_u8, glow::STATIC_DRAW);
+    }
 
     // We now construct a vertex array to describe the format of the input buffer
-    let vao = gl.create_vertex_array().unwrap();
-    gl.bind_vertex_array(Some(vao));
-    gl.enable_vertex_attrib_array(0);
-    gl.vertex_attrib_pointer_f32(0, 3, glow::FLOAT, false, 12, 0);
+    let vao = unsafe { gl.create_vertex_array().unwrap() };
+    unsafe {
+        gl.bind_vertex_array(Some(vao));
+        gl.enable_vertex_attrib_array(0);
+        gl.vertex_attrib_pointer_f32(0, 3, glow::FLOAT, false, 12, 0);
+    }
 
     (vbo, vao)
 }
 
-unsafe fn set_uniform(gl: &glow::Context, program: NativeProgram) {
-    let uniform_location = gl.get_uniform_location(program, "transform");
-    gl.uniform_matrix_4_f32_slice(
+fn set_uniform_matrix(gl: &glow::Context, program: NativeProgram, name: &str, matrix: &[f32]) {
+    let uniform_location = unsafe { gl.get_uniform_location(program, name) };
+    unsafe { gl.uniform_matrix_4_f32_slice(
         uniform_location.as_ref(),
         false,
-        &[1.0f32, 0., 0., 0., 0., 1., 0., 0., 0., 0., 1., 0., 0., 0., 0., 1.],
-    );
+        matrix,
+    ); }
 }
 
 const VERTEX_SHADER_SOURCE: &str = r#"#version 150
