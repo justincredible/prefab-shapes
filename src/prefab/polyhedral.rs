9971b780380f3a4d3c48052157dc311d6a14c099
Justin Symonds <justinjfsymonds@hotmail.com>
2026-06-17 18:12:56 -0700

    Rename local variable

--- src/prefab/polyhedral.rs
+++ src/prefab/polyhedral.rs
@@ -38,14 +38,14 @@ pub(super) fn platonic_solid(solid: &(impl Polyhedral + ?Sized)) -> HashSet<Vec<
 }
 
 fn find_face(faces: &mut HashSet<Vec<usize>>, edges: &Vec<Vec<usize>>, target: usize, mut current: Vec<usize>) {
-    let last = current[current.len()-1];
+    let previous = current[current.len()-1];
     if current.len() == target {
-        if edges[last].contains(&current[0]) {
+        if edges[previous].contains(&current[0]) {
             current.sort();
             faces.insert(current);
         }
     } else if current.len() < target {
-        for i in edges[last].iter().map(Clone::clone).filter(|x| !current.contains(x)) {
+        for i in edges[previous].iter().map(Clone::clone).filter(|x| !current.contains(x)) {
             let mut next = current.clone();
             next.push(i);
             find_face(faces, edges, target, next);
